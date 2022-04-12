b5f5fcd28d7cac1625869b4df5be1c6f144f7a41
Lilly Pernichele <89110872+foolbuffoon@users.noreply.github.com>
2022-04-12 12:36:40 -0600

    updated character class(unfinished?)
    should we have the option to change locations in the character class instead of in the logic class? it feels like this class is a little light on methods tbh

--- src/character.py
+++ src/character.py
@@ -1,1 +1,14 @@
-
+class Character:
+    playerHealth = 0
+    playerInventorySpace = 0
+    playerInventory = []
+    victory = False
+    playerMoney = 0
+    alive = True
+    def __init__(self):
+        self.playerHealth = 100
+        self.playerInventorySpace = 10
+        self.playerInventory = []
+        self.victory = False
+        self.playerMoney = 0
+        self.alive = True
