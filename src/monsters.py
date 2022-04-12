08ab40527918a1893f7373459873a15b08dd33e2
NikolasKath <89263739+NikolasKath@users.noreply.github.com>
2022-04-12 12:30:33 -0600

    Update monsters.py

--- src/monsters.py
+++ src/monsters.py
@@ -1,1 +1,17 @@
+import monster,Damage,health,Main Boss
+
+
+## Monster
+setDamage(int)
+setHealth(int)
+setSize(int)
+healthStart(boolean)
+monsterSize(boolean)
+
+##Main Boss
+setDamage(int)
+sethealth(int)
+setSize(int)
+healthStart(boolean)
+monsterSize(boolean)
 
