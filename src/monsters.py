b7a4976523d8731972c78bfc6f91beb3835d4f37
Lilly Pernichele <89110872+foolbuffoon@users.noreply.github.com>
2022-04-13 10:05:35 -0600

    Update monsters.py

--- src/monsters.py
+++ src/monsters.py
@@ -1,14 +1,9 @@
-## Monster
-setDamage(int)
-setHealth(int)
-setSize(int)
-healthStart(bool)
-monsterSize(bool)
+class Monster:
+    setDamage = 0
+    sethealth = 0
+    setAttackTimer = 0
 
-##Main Boss
-setDamage(int)
-sethealth(int)
-setSize(int)
-healthStart(bool)
-monsterSize(bool)
-
+    def __init__(self):
+        self.setDamage = 0
+        self.sethealth = 0 
+        self.setAttackTimer = 0
