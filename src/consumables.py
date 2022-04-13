cf5ab98dc60cfc61bb60e4ef65b01005d097f5d0
Lilly Pernichele <89110872+foolbuffoon@users.noreply.github.com>
2022-04-12 17:12:34 -0700

    Update consumables.py
    we might need to add the functions for what the different consumable types do(there's gonna be one that adds strength, one that heals the player, and one that lets the player cast more spells, but i dont really know how to do that, will ask kapptie abt it thursday.

--- src/consumables.py
+++ src/consumables.py
@@ -1,1 +1,15 @@
+class Consumables:
+    consumeType = 0
+
+
+    def __init__(self):
+        consumeType = 0
+
+    def typeSet(self):
+        consumeType = 1
+        return consumeType
+        #consumeType = 2
+        #return consumeType
+        #consumeType = 3
+        #return consumeType
 
