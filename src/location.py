3ac70488b7957e55c36082395172f2f0056f7229
Lilly Pernichele <89110872+foolbuffoon@users.noreply.github.com>
2022-04-12 17:01:12 -0700

    Update location.py

--- src/location.py
+++ src/location.py
@@ -1,8 +1,23 @@
-locName(str)
-locStart(bool)
-locEnd(bool)
-locHasMonsters(bool)
-locHasItems(bool)
-locGet(array)
-locGo(array)
-locFlavor(str)
+class Location: 
+locName = ""
+locHasMonsters = False 
+locHasItems = False 
+locGet = ["placeholder1", "placeholder2"]
+locGo = ["placeholder3", "placeholder4", "placeholder5"]
+locFlavor = ""
+
+def __init__(self):
+    self.locName = "test"
+    self.locHasMonsters = True
+    self.locHasItems = True
+    self.locGet[0] = "a location"
+    self.locGet[1] = "another location"
+    self.locGo[0] = "a new place"
+    self.locGo[1] = "a different place"
+    self.locGo[2] = "a third place! wowee!"
+    self.locFlavor = "bitter, with notes of cellulose and TV static"
+
+def travel(self, locGo, locGet):
+
+    return self.locName;
+    
