3dfef3183ca4af94b759e15abd41ca569c46af39
LemScoot <89105667+LemScoot@users.noreply.github.com>
2022-04-14 11:36:15 -0600

    Update spells.py

--- src/spells.py
+++ src/spells.py
@@ -1,1 +1,14 @@
+class Spells:
+    spellType = 0
 
+
+    def __init__(self):
+        spellType = 0
+
+    def typeSet(self):
+        spellType = 1
+        return spellType
+        #spellType = 2
+        #return spellType
+        #spellType = 3
+        #return spellType
