44763fe002cba9094811fa6cdeb0242ec16e0f85
Lilly Pernichele <89110872+foolbuffoon@users.noreply.github.com>
2022-04-14 12:33:50 -0600

    MAJOR UPDATE TO LOGIC.PY
    download me!!

--- src/Logic.py
+++ src/Logic.py
@@ -1,1 +1,33 @@
-locTravel():
+# import overhead
+from character import Character
+from location import Location
+from monsters import Monster
+from spells import Spells
+from weapons import Weapons
+gameStart = False
+# calling the class objects
+player = Character()
+locationtest = Location()
+monstertest = Monster()
+weapontest = Weapons()
+spellstest = Spells()
+
+art_logo = (r"""
+ __                               _                 _
+/ _\_      ____ _ _ __ ___  _ __ | | __ _ _ __   __| |
+\ \\ \ /\ / / _` | '_ ` _ \| '_ \| |/ _` | '_ \ / _` |
+_\ \\ V  V / (_| | | | | | | |_) | | (_| | | | | (_| |
+\__/ \_/\_/ \__,_|_| |_| |_| .__/|_|\__,_|_| |_|\__,_|
+                           |_|
+""")
+print(art_logo)
+print("Welcome to Swampland, a text-based adventure game made by Lilly Pernichele, Zack Poulson, Liam Scott, and Nikolas Kath")
+startinput = input("Press enter to begin")
+if startinput == "":
+    gameStart = True
+
+while gameStart == True:
+    print('You begin your journey...')
+    print('You awake in a swamp, with sword in your hand and no idea of who you are in your head')
+    break
+gameStart = False
