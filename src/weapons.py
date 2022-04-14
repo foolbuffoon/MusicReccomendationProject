2a6f726e4db9247dc73fefec589afe0263516a8c
Lilly Pernichele <89110872+foolbuffoon@users.noreply.github.com>
2022-04-14 11:21:15 -0600

    Update weapons.py

--- src/weapons.py
+++ src/weapons.py
@@ -1,1 +1,9 @@
+class Weapons:
+    setDamage = 0
+    setDurability = 0
+    damageType = ''
 
+    def __init__(self):
+        self.setDamage = 0
+        self.setDurability = 0
+        self.damageType = ''
